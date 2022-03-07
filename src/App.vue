3b2b9183a577f34a93d92411201f34f1c1f2cdbe
maryanna <prelecnaja@gmail.com>
2022-03-07 22:10:18 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <fragment>
+    <div class="wrapper">
+      <input v-model="sourceAmount" type="number" class="border"/>
+      <select
+        class="select border"
+        name="select"
+        v-model="sourceCurrency"
+        @change="countOutputAmount"
+      >
+        <option v-for="item in currencyItems" v-bind:key="item">
+          {{ item }}
+        </option>
+      </select>
+      <input type="number" v-model="outputAmount" class="border"/>
+      <select
+        class="select border"
+        name="select"
+        v-model="outputCurrency"
+        @change="countSourceAmount"
+      >
+        <option v-for="item in currencyItems" v-bind:key="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+  </fragment>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      sourceAmount: '0',
+      sourceCurrency: '',
+      outputAmount: '0',
+      outputCurrency: '',
+      currency: {},
+      currencyItems: [],
+    };
+  },
+  methods: {
+    async getData() {
+      let response = await fetch(
+        'https://freecurrencyapi.net/api/v2/latest?apikey=9F9ek9MUGlgme5ZL0aXF4uPvcfMeldaeyJs1HNWO', {mode: 'no-cors'}
+      );
+      let json = await response.json();
+      this.currency = json.data;
+      console.log(Object.keys(this.currency));
+      this.currencyItems = Object.keys(this.currency);
+      this.sourceCurrency = this.currencyItems[138];
+      this.outputCurrency = this.currencyItems[45];
+      console.log(this.currencyItems.sort());
+      return json;
+    },
+
+    countSourceAmount() {
+      const sourceCurrencyValue = this.currency[this.sourceCurrency];
+      const outputCurrencyValue = this.currency[this.outputCurrency];
+
+      this.outputAmount = (
+        this.sourceAmount /
+        (sourceCurrencyValue / outputCurrencyValue)
+      ).toFixed(2);
+      console.log(this.outputAmount);
+    },
+
+    countOutputAmount() {
+      const sourceCurrencyValue = this.currency[this.sourceCurrency];
+      const outputCurrencyValue = this.currency[this.outputCurrency];
+
+      this.sourceAmount = (
+        this.outputAmount *
+        (sourceCurrencyValue / outputCurrencyValue)
+      ).toFixed(2);
+      console.log(this.sourceAmount);
+    },
+  },
+
+  mounted() {
+    this.getData();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.wrapper {
+  display: grid;
+  grid-template-columns: 200px 200px;
+  grid-column-gap: 10px;
+  grid-row-gap: 1em;
+}
+.select {
+  width: 100px;
+  padding-bottom: 10px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  line-height: 30px;
+}
+.button {
+  margin-top: 10px;
+}
+.border {
+  border-radius: 5px;
+  border: solid 2px gray;
 }
 </style>
